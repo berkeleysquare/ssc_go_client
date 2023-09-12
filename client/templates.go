b08bcf494bc08905ca965c23dc184dd9f0b0fff6
berkeleysquare <johnk@spectralogic.com>
2023-09-12 16:05:09 -0600

    Fetch manifest

--- client/templates.go
+++ client/templates.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"encoding/csv"
-	"encoding/json"
 	"fmt"
 	"github.com/SpectraLogic/ssc_go_client/openapi"
 	"html/template"
@@ -11,7 +10,6 @@ import (
 )
 
 func writeBreadcrumbs(ssc *SscClient, args *Arguments) error {
-
 	// fileName runs once, inputFile iterates through CSV
 	if len(args.Job) == 0 {
 		return fmt.Errorf("no job name specified")
@@ -40,12 +38,6 @@ func writeBreadcrumbs(ssc *SscClient, args *Arguments) error {
 		return fmt.Errorf("could not print manifest header %v", err)
 	}
 
-	/**
-		wOut := os.Stdout
-		w := bufio.NewWriter(wOut)
-		defer w.Flush()
-	**/
-
 	tmpl := template.Must(template.ParseFiles(args.InputFile))
 
 	// iterate through all pages and extract just names
@@ -55,12 +47,12 @@ func writeBreadcrumbs(ssc *SscClient, args *Arguments) error {
 		log.Printf("doGetManifest(%s)", args.Job)
 	}
 	for more {
-		ret, isTruncated, err := doGetBogusManifest(ssc, args.Job, offset, verbose)
+		ret, isTruncated, err := doGetManifest(ssc, args.Job, offset, verbose)
 		if err != nil {
 			return fmt.Errorf("get manifest %s failed %v\n", args.Job, err)
 		}
 
-		err = doBreadcrumbs(tmpl, ret)
+		err = doBreadcrumbs(tmpl, ret, verbose)
 		if err != nil {
 			return fmt.Errorf("could not list search results %v\n", err)
 		}
@@ -71,52 +63,15 @@ func writeBreadcrumbs(ssc *SscClient, args *Arguments) error {
 	return nil
 }
 
-func doGetBogusManifest(ssc *SscClient, job string, offset int64, verbose bool) ([]openapi.ApiManifestFile, bool, error) {
-
-	bogus := `[
-  {
-    "path": "\\\\localhost\\c$\\StorCycle\\shares\\delayedAction\\coffeehouse\\jk\\IHaveDreamed.mp3",
-    "name": "IHaveDreamed.mp3",
-    "size": 1525449,
-    "recordTime": "2023-08-25T20:56:18.143Z",
-    "hash": "b2b256.9d7fa6ebee83b1ee72d7a9da8f4f02f35887f4c26d5d3992365a95e2c9982588"
-  },
-  {
-    "path": "\\\\localhost\\c$\\StorCycle\\shares\\delayedAction\\FarAwayPlaces.jpg",
-    "name": "FarAwayPlaces.jpg",
-    "size": 62061,
-    "recordTime": "2023-08-25T20:56:18.143Z",
-    "hash": "b2b256.7b79177df8f840b2ebb06599af9fbbb6aef2d4431d63ec86cf187e1a3149c9d7"
-  },
-  {
-    "path": "\\\\localhost\\c$\\StorCycle\\shares\\delayedAction\\coffeehouse\\IsYouIsOrIsYouAint.mp3",
-    "name": "IsYouIsOrIsYouAint.mp3",
-    "size": 2310657,
-    "recordTime": "2023-08-25T20:56:18.144Z",
-    "hash": "b2b256.bb0ebc9ade6d905decdaec5a4f03392b6e3d8ff5ca2220208cbeae6a0b2cd24b"
-  }
-]`
-	isTruncated := false
-
-	var files []openapi.ApiManifestFile
-
-	err := json.Unmarshal([]byte(bogus), &files)
-	if err != nil {
-		return nil, isTruncated, err
-	}
-
-	if verbose {
-		log.Printf("Manifest %s: TotalResults %d, offset %d, limit %d", job, len(files), offset, limit)
-	}
-	return files, isTruncated, nil
-}
-
-func doBreadcrumbs(tmpl *template.Template, files []openapi.ApiManifestFile) error {
+func doBreadcrumbs(tmpl *template.Template, files []openapi.ApiManifestFile, verbose bool) error {
 
 	for fileIndex := range files {
 		file := files[fileIndex]
 		var f *os.File
 		fpath := *file.Path + ".html"
+		if verbose {
+			log.Printf("makeBreadcrumb %s\n", fpath)
+		}
 		f, err := os.Create(fpath)
 		if err != nil {
 			return fmt.Errorf("Failed to create file %s\n%v", fpath, err)
